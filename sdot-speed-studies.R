5af8b0cddacdfd7d1aa2f18f7f329c2a6e79e938
Tiernan Martin <tiernan.martin@gmail.com>
2021-05-22 23:16:36 -0700

    create steps for extracting data from one pdf

---
+++ sdot-speed-studies.R
@@ -0,0 +1,136 @@
+
+# SETUP -------------------------------------------------------------------
+
+library(tidyverse)
+library(pdftools)
+library(janitor)
+library(lubridate)
+
+
+
+# CREATE THE PDFTOOLS FUNCTION -------------------------------------------
+
+fp <- "data/SDOT-Speed-Studies/2019_01_03_10932_SGT.pdf"
+
+txt <- pdf_text(fp) %>% 
+  pluck(3) %>% 
+  str_remove_all("\\\n")
+
+
+### LOCATION
+
+txt_location <- pdf_text(fp) %>% 
+  pluck(3) %>% 
+  str_split("\\\n") %>% 
+  pluck(1) %>% 
+  head(3) %>% 
+  str_extract(".*?(?=\\s{20,})") %>% 
+  str_squish() %>% 
+  str_remove("ON STREET : |LOCATED : |CROSS STREEET : ")
+  
+tbl_location <- txt_location %>% 
+  matrix(ncol = 3) %>% 
+  as_tibble() %>% 
+  `colnames<-`(c("street","xstreet_dir","xstreet")) %>% 
+  mutate(location = str_c(street, xstreet_dir, xstreet, sep = " "))
+
+### DATE AND TIME
+
+txt_datetime <- pdf_text(fp) %>% 
+  pluck(3) %>% 
+  str_split("\\\n") %>% 
+  pluck(1) %>% 
+  head(3) %>% 
+  str_extract("(?<=\\s{20}).*$") %>% 
+  str_remove("-") %>% 
+  str_remove("Site: ") %>% 
+  str_squish() %>% 
+  str_remove("ON STREET : |LOCATED : |CROSS STREEET : ")
+
+
+create_datetime <- function(x){strptime(x, "%A, %Om/%d/%Y %I:%M %p")}
+
+tbl_datetime <- txt_datetime %>% 
+  matrix(ncol = 3) %>% 
+  as_tibble() %>% 
+  `colnames<-`(c("site","datetime_start","datetime_end")) %>% 
+  transmute(
+    site,
+    datetime_start = create_datetime(datetime_start),
+    datetime_end = create_datetime(datetime_end)) %>% 
+  mutate(
+    date_start = lubridate::as_date(datetime_start),
+    date_end = lubridate::as_date(datetime_end),
+    time_start = hms::as_hms(datetime_start),
+    time_end = hms::as_hms(datetime_end)
+    )
+
+### AVG + MIN + MAX SPEEDS
+
+txt_avg <- str_extract(txt,"(?<=Average\\s\\(Mean\\)\\s).*?(?=\\smph)") %>%  
+  as.numeric()
+
+txt_min <- str_extract(txt,"(?<=Minimum\\s).*?(?=\\smph)") %>% 
+  as.numeric()
+
+txt_max <- str_extract(txt,"(?<=Maximum\\s).*?(?=\\smph)") %>% 
+  as.numeric()
+
+### SPEED PERCENTILE
+
+txt_pctile <- str_extract(txt,"(?<=Percentile Speeds).*?(?=Speeds Exceeded)") %>% 
+  str_remove_all("\\(mph\\)") %>% 
+  str_squish() %>% 
+  str_split(" ") %>% 
+  pluck(1)
+
+tbl_pctile <- tibble(txt_pctile[6:10]) %>% 
+  t() %>% 
+  `colnames<-`(txt_pctile[1:5]) %>%
+  as_tibble() %>% 
+  clean_names()
+
+
+### SPEEDS EXCEEDED
+
+txt_speedexc <- str_extract(txt,"(?<=Speeds Exceeded).*?(?=Study Grand Totals)") %>% 
+  str_remove_all("mph") %>%
+  str_remove_all("%") %>% 
+  str_squish() %>% 
+  str_split(" ") %>% 
+  pluck(1)
+
+rn_speedexc <- txt_speedexc[1:6]
+
+
+tbl_speedexc <- txt_speedexc[7:length(txt_speedexc)] %>% 
+  matrix(nrow = 2) %>% 
+  as_tibble %>% 
+  `colnames<-`(rn_speedexc) %>% 
+  rename_all(.funs = ~str_c("pctile_",.)) %>% 
+  gather() %>% 
+  mutate(key2 = case_when(
+    str_detect(value,"\\(") ~ "count",
+    TRUE ~ "pct"
+  )) %>% 
+  mutate(value = str_remove_all(value,"\\(|\\)")) %>% 
+  unite("key",c("key","key2")) %>% 
+  pivot_wider(names_from = "key", values_from = "value")
+
+### TOTAL
+
+tbl_total <- txt %>% 
+  str_extract("Combined\\s+\\d+") %>% 
+  str_extract("\\d+") %>% 
+  as_tibble() %>% 
+  `colnames<-`("total_vehicles")
+
+
+### CREATE TIBBLE
+
+list(tbl_location, 
+     tbl_datetime,
+     tbl_pctile, 
+     tbl_speedexc,
+     tbl_total) %>% 
+  map_dfc(c) %>% View()
